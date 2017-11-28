7d25629a288c62545a19c06e0f64f8e3115ba768
Fabian Lehmann <mail.fabian.lehmann@gmail.com>
2017-11-28 10:51:16 +0000

    Cloud Foundry Setup

--- ui/resources/Util.js
+++ ui/resources/Util.js
@@ -54,3 +54,20 @@ function openFirstDialog() {
       oFirstDialog.open();
    }
 }
+
+$(function() {
+	$.ajax({
+		type: 'GET',
+		url: '/',
+		headers: {'X-Csrf-Token': 'Fetch'},
+		success: function(res, status, xhr) {
+			var sHeaderCsrfToken = 'X-Csrf-Token';
+			var sCsrfToken = xhr.getResponseHeader(sHeaderCsrfToken);
+			$(document).ajaxSend(function(event, jqxhr, settings) {
+				if(settings.type === 'POST' || settings.type === 'PUT' || settings.type === 'DELETE') {
+					jqxhr.setRequestHeader(sHeaderCsrfToken, sCsrfToken);
+				}
+			});
+		}
+	});
+});
